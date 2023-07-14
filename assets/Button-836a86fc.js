36717312e0d4e109fffe7b67343977e36d695ce1
darcrand <darcrandex@gmail.com>
2023-07-14 11:23:05 +0800

    Updates

---
+++ assets/Button-836a86fc.js
@@ -0,0 +1,5 @@
+import{j as o,a as c,F as s}from"./jsx-runtime-91a467a5.js";import{B as a}from"./Button-adfefb74.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";function e(n){const t=Object.assign({h1:"h1",pre:"pre",code:"code"},r(),n.components);return c(s,{children:[o(t.h1,{id:"button-doc",children:"Button Doc"}),`
+`,o(t.pre,{children:o(t.code,{children:`<Button></Button>
+`})}),`
+`,o(a,{label:"123"})]})}function h(n={}){const{wrapper:t}=Object.assign({},r(),n.components);return t?o(t,Object.assign({},n,{children:o(e,n)})):e(n)}export{h as default};
+//# sourceMappingURL=Button-836a86fc.js.map
---
+++ components/Button/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @name Button
+ * @description
+ * @author darcrand
+ */
+import { PropsWithChildren } from 'react';
+export type ButtonProps = PropsWithChildren<{
+    className?: string;
+}>;
+export default function Button(props: ButtonProps): JSX.Element;
